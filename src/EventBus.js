238f66844f6f6640832e182fd5804b3b8a410619
VictorHazebrouck <hazebrouck.victor@gmail.com>
2024-07-09 11:12:04 +0200

    added event bus and types

---
+++ src/EventBus.js
@@ -0,0 +1,26 @@
+/** @type {import("./types/eventBus").EventBus} */
+const EventBus = {
+    _listeners: {},
+
+    subscribe(event, listener) {
+        if (!this._listeners[event]) {
+            this._listeners[event] = [];
+        }
+        this._listeners[event].push(listener);
+    },
+
+    unsubscribe(event, listener) {
+        if (!this._listeners[event]) {
+            return;
+        }
+        this._listeners[event] = this._listeners[event].filter((l) => l !== listener);
+    },
+
+    publish(event, data) {
+        if (!this._listeners[event]) return;
+
+        this._listeners[event].forEach((listener) => listener(data));
+    },
+};
+
+export default EventBus;
---
+++ src/types/eventBus.ts
@@ -0,0 +1,39 @@
+/** List of all event names with their corresponding data to be passsed around */
+type EventTypes = {
+    test: string;
+    haha: number;
+};
+
+type Listener<T> = (data: T) => void;
+
+export interface EventBus {
+    /**
+     * The list of subscribed events
+     * @private
+     */
+    _listeners: Record<string, Listener<any>[]>;
+
+    /**
+     * Subscribe a callback function to an event.
+     *
+     * @param event - The event name.
+     * @param listener - The callback function to handle the event.
+     */
+    subscribe<E extends keyof EventTypes>(event: E, listener: Listener<EventTypes[E]>): void;
+
+    /**
+     * Unsubscribe a callback function from an event.
+     *
+     * @param event - The event name.
+     * @param listener - The callback function to handle the event.
+     */
+    unsubscribe<E extends keyof EventTypes>(event: E, listener: Listener<EventTypes[E]>): void;
+
+    /**
+     * Publish an event.
+     *
+     * @param event - The event name.
+     * @param listener - The data being shared to subscribers
+     */
+    publish<E extends keyof EventTypes>(event: E, data: EventTypes[E]): void;
+}
